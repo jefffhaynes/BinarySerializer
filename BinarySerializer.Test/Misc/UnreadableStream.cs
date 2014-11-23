727ddc33a9134a5bc63a215c0ab793f77d8b1d54
jefffhaynes <feedyurhed@gmail.com>
2014-11-23 16:35:50 -0500

    Removed old files

---
+++ BinarySerializer.Test/Misc/UnreadableStream.cs
@@ -0,0 +1,54 @@
+using System;
+using System.IO;
+
+namespace BinarySerializer.Test.Misc
+{
+    internal class UnreadableStream : Stream
+    {
+        public override void Flush()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            throw new IOException();
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+        }
+
+        public override bool CanRead
+        {
+            get { return true; }
+        }
+
+        public override bool CanSeek
+        {
+            get { return false; }
+        }
+
+        public override bool CanWrite
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override long Length
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override long Position { get; set; }
+    }
+}
