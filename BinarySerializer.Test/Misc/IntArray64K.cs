260ddc2b1e45faba45ed77d704fe57e7afe8c525
jefffhaynes <feedyurhed@gmail.com>
2015-01-02 12:44:36 -0500

    Added large array test

---
+++ BinarySerializer.Test/Misc/IntArray64K.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BinarySerialization;
+
+namespace BinarySerializer.Test.Misc
+{
+    public class IntArray64K
+    {
+        [FieldLength(65536 * sizeof(int))]
+        public int[] Array { get; set; }
+    }
+}
---
+++ BinarySerializer.Test/Misc/LargeArrayTests.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BinarySerializer.Test.Misc
+{
+    [TestClass]
+    public class LargeArrayTests
+    {
+        [TestMethod]
+        public void LargeArrayTest()
+        {
+            var ser = new BinarySerialization.BinarySerializer();
+            var data = new byte[65536 * sizeof(int)];
+
+            var des0 = ser.Deserialize<IntArray64K>(data);
+
+            using (var ms = new MemoryStream(data))
+            {
+                var des = ser.Deserialize<IntArray64K>(ms);
+            }
+        }
+    }
+}
