7fc340b1f71ed7dfd1c310bf6c9f721c0a5f5558
jefffhaynes <feedyurhed@gmail.com>
2015-01-11 23:20:01 -0500

    missing file

---
+++ BinarySerializer/Graph/ConditionalBinding.cs
@@ -0,0 +1,13 @@
+﻿namespace BinarySerialization.Graph
+{
+    internal class ConditionalBinding : Binding
+    {
+        public ConditionalBinding(SerializeWhenAttribute attribute, int level)
+            : base(attribute, level)
+        {
+            ConditionalValue = attribute.Value;
+        }
+
+        public object ConditionalValue { get; private set; }
+    }
+}
