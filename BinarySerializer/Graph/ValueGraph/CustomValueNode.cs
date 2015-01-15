f1b6cf9f1dbe176133cae2a98d731b213f178f4c
jefffhaynes <feedyurhed@gmail.com>
2015-01-14 22:24:32 -0500

    missing file

---
+++ BinarySerializer/Graph/ValueGraph/CustomValueNode.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using BinarySerialization.Graph.TypeGraph;
+
+namespace BinarySerialization.Graph.ValueGraph
+{
+    internal class CustomValueNode : ValueNode
+    {
+        public CustomValueNode(Node parent, string name, TypeNode typeNode) : base(parent, name, typeNode)
+        {
+        }
+
+        public override object Value { get; set; }
+        protected override void SerializeOverride(Stream stream, EventShuttle eventShuttle)
+        {
+            var serializationContext = CreateSerializationContext();
+            var value = (IBinarySerializable)Value;
+            value.Serialize(stream, Endianness, serializationContext);
+        }
+
+        public override void DeserializeOverride(StreamLimiter stream, EventShuttle eventShuttle)
+        {
+            var serializationContext = CreateSerializationContext();
+            var value = (IBinarySerializable)Value;
+            value.Deserialize(stream, Endianness, serializationContext);
+        }
+    }
+}
