2310e4d0ae8950c1665a87678fef21891d23081d
jefffhaynes <feedyurhed@gmail.com>
2015-01-24 16:13:05 -0500

    cleanup

--- BinarySerializer.Test/Custom/CustomTests.cs
+++ BinarySerializer.Test/Custom/CustomTests.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BinarySerializer.Test.Custom
 {
