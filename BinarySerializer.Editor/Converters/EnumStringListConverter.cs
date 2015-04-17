bb0ef29ae6fff8e7be280fb48ddc042c5a861784
jefffhaynes <feedyurhed@gmail.com>
2015-04-17 13:31:09 -0400

    enum changes

---
+++ BinarySerializer.Editor/Converters/EnumStringListConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace BinarySerializer.Editor.Converters
+{
+    public class EnumStringListConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var enums = (IEnumerable<Enum>) value;
+            var strings = enums.Select(e => string.Format("0x{0} -> {1}", System.Convert.ToInt64(e).ToString("X2"), e.ToString()));
+            return string.Join(", ", strings);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
