fb760d20870ac45fa5e40eae66c53acb7f9521c8
jefffhaynes <feedyurhed@gmail.com>
2015-04-19 15:23:41 -0400

    3.9.2

--- BinarySerializer/Properties/AssemblyInfo.cs
+++ BinarySerializer/Properties/AssemblyInfo.cs
@@ -23,8 +23,8 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.9.0.0")]
-[assembly: AssemblyFileVersion("3.9.0.0")]
+[assembly: AssemblyVersion("3.9.2.0")]
+[assembly: AssemblyFileVersion("3.9.2.0")]
 [assembly: AssemblyInformationalVersion("3.9.0")]
 
 
