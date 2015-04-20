d4472a34d59152b72ce8447571cfe19c881811ae
jefffhaynes <feedyurhed@gmail.com>
2015-04-19 23:21:28 -0400

    additional order tests

--- BinarySerializer.Test/Order/OrderTests.cs
+++ BinarySerializer.Test/Order/OrderTests.cs
@@ -9,8 +9,8 @@ namespace BinarySerialization.Test.Order
         [TestMethod]
         public void OrderTest()
         {
-            var order = new OrderClass {First = 1, Second = 2};
-            Roundtrip(order, new byte[] {0x1, 0x2});
+            var order = new OrderClass {First = 1, Second = 2, Name = "Alice"};
+            Roundtrip(order, new byte[] {0x1, 0x2, 0x5, 0x41, 0x6c, 0x69, 0x63, 0x65});
         }
 
         [TestMethod]
