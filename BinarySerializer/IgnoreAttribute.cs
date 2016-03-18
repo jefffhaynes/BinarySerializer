a3b220423aca23460d184f94fa7a67bc4b28cc20
jefffhaynes <feedyurhed@gmail.com>
2016-03-18 16:52:48 -0400

    fixed comment

--- BinarySerializer/IgnoreAttribute.cs
+++ BinarySerializer/IgnoreAttribute.cs
@@ -3,8 +3,7 @@
 namespace BinarySerialization
 {
     /// <summary>
-    /// Instructs the <see cref="BinarySerializer.Serialize"/> method and the <see cref="BinarySerializer.Deserialize"/> 
-    /// of the <see cref="BinarySerializer"/> not to serialize the public field or public read/write property value.
+    /// Instructs <see cref="BinarySerializer"/> not to serialize or deserialize the public field or property value.
     /// </summary>
     [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
     public sealed class IgnoreAttribute : Attribute
