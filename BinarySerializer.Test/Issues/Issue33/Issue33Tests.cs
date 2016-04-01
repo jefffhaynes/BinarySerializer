8e7359dcdd739d5d0a4e6b549eddfb0d4f2bf692
jefffhaynes <feedyurhed@gmail.com>
2016-04-01 10:16:57 -0400

    Fixes issue #33 by applying null-terminated string length check even when explicitly specified

---
+++ BinarySerializer.Test/Issues/Issue33/Issue33Tests.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BinarySerialization.Test.Issues.Issue33
+{
+    [TestClass]
+    public class Issue33Tests
+    {
+        [TestMethod]
+        public void DeserializeMessage()
+        {
+            var serializer = new BinarySerializer() { Endianness = BinarySerialization.Endianness.Little };
+            var inBytes = new byte[]
+                              {
+                                  0xFE, 0x31, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                                  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02
+                              };
+            var expectedObj = new Bin3Data() { BinType = 254, Ident = "1", Occupancy = BinOccupancy.Full };
+
+            Bin3Data actualObj;
+            using (var stream = new MemoryStream(inBytes))
+            {
+                actualObj = serializer.Deserialize<Bin3Data>(stream);
+            }
+
+            Assert.AreEqual(expectedObj.BinType, actualObj.BinType);
+            Assert.AreEqual(expectedObj.Ident, actualObj.Ident);
+            Assert.AreEqual(expectedObj.Occupancy, actualObj.Occupancy);
+        }
+    }
+}
