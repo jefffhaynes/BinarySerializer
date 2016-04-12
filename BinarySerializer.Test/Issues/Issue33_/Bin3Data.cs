f01549233326b43cd54c4269b752aa000e00a018
jefffhaynes <feedyurhed@gmail.com>
2016-04-12 18:36:36 -0400

    adding issue 33 temp

---
+++ BinarySerializer.Test/Issues/Issue33_/Bin3Data.cs
@@ -0,0 +1,44 @@
+﻿namespace BinarySerialization.Test.Issues.Issue33
+{
+    class Bin3Data
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Bin3Data"/> class.
+        /// </summary>
+        public Bin3Data()
+            : base()
+        {
+        }
+
+        /// <summary>
+        /// Gets or sets type of the bin
+        /// </summary>
+        [FieldOrder(0)]
+        [SerializeAs(SerializedType = SerializedType.UInt1)]
+        public byte BinType { get; set; }
+
+        /// <summary>
+        /// Gets or sets ident
+        /// </summary>
+        [FieldOrder(1)]
+        [FieldLength(31)]
+        [SerializeAs(SerializedType = SerializedType.NullTerminatedString)]
+        public string Ident { get; set; }
+
+        /// <summary>
+        /// Gets or sets Occupancy
+        /// </summary>
+        [FieldOrder(2)]
+        [SerializeAs(SerializedType = SerializedType.UInt1)]
+        public BinOccupancy Occupancy { get; set; }
+
+
+        /// <summary>
+        /// Gets or sets Occupancy
+        /// </summary>
+        [FieldOrder(3)]
+        [FieldLength(6)]
+        [SerializeAs(SerializedType = SerializedType.NullTerminatedString)]
+        public BinOccupancy OccupancyString { get; set; }
+    }
+}
---
+++ BinarySerializer.Test/Issues/Issue33_/BinOccupancy.cs
@@ -0,0 +1,8 @@
+﻿namespace BinarySerialization.Test.Issues.Issue33
+{
+    public enum BinOccupancy : byte
+    {
+        Empty = 0x00,
+        Full = 0x02
+    }
+}
