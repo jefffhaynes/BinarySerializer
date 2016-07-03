331efbb9007a673c4458462d5fc4c1976c1b5db4
Jason Adams <jda808@love.pandacorn.com>
2016-07-03 02:56:08 -0500

    WIP from MacOSX

---
+++ test/BinarySerializer.Tests/SampleTest.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BinarySerializer.Tests
+{
+    // see example explanation on xUnit.net website:
+    // https://xunit.github.io/docs/getting-started-dotnet-core.html
+    public class SampleTest
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
