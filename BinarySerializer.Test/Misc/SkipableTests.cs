294932936684787376cbcde738ceac560248dad6
jefffhaynes <feedyurhed@gmail.com>
2017-01-03 23:12:48 -0500

    nullable test

---
+++ BinarySerializer.Test/Misc/SkipableTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BinarySerialization.Test.Misc
+{
+    [TestClass]
+    public class SkipableTests : TestBase
+    {
+        public void SkipTest()
+        {
+            var actual = Deserialize<SkipableContainerClass>(new byte[0]);
+            Assert.IsNull(actual);
+        }
+    }
+}
