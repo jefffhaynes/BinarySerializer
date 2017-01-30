6209617b759c1d12b2934f6203a98f5d7cd80a1d
jefffhaynes <feedyurhed@gmail.com>
2017-01-29 19:10:08 -0500

    cleanup of package info

---
+++ BinarySerializer.Console/Beer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using BinarySerialization;
+
+namespace BinarySerializer.Console
+{
+    public class Beer
+    {
+        [SerializeAs(SerializedType.LengthPrefixedString)]
+        [FieldOrder(0)]
+        public string Brand;
+        
+        [FieldOrder(1)]
+        public byte SortCount;
+
+        [FieldOrder(2)]
+        [FieldCount("SortCount")]
+        public List<SortContainer> Sort;
+
+        [FieldOrder(3)]
+        public float Alcohol;
+
+        [SerializeAs(SerializedType.LengthPrefixedString)]
+        [FieldOrder(4)]
+        public string Brewery;
+    }
+    
+    public class SortContainer
+    {
+        [FieldOrder(0)]
+        public byte NameLength;
+
+        [FieldOrder(1)]
+        [FieldLength("NameLength")]
+        public string Name;
+    }
+}
