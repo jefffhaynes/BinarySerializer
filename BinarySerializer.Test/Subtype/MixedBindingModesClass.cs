e6829722a85e18528f67a765db73df8a2f5b62c5
jefffhaynes <feedyurhed@gmail.com>
2017-06-03 11:03:37 -0400

    fixed subtype test

--- BinarySerializer.Test/Subtype/MixedBindingModesClass.cs
+++ BinarySerializer.Test/Subtype/MixedBindingModesClass.cs
@@ -9,7 +9,7 @@
         [Subtype("Indicator", 1, typeof(SubclassB), BindingMode = BindingMode.OneWay)]
         [Subtype("Indicator", 4, typeof(SubclassB), BindingMode = BindingMode.OneWay)]
         [Subtype("Indicator", 2, typeof(SubclassA), BindingMode = BindingMode.OneWay)]
-        [Subtype("Indicator", 2, typeof(SubclassB))]
+        [Subtype("Indicator", 2, typeof(SubclassB), BindingMode = BindingMode.OneWayToSource)]
         [Subtype("Indicator", 3, typeof(SubSubclassC))]
         public Superclass Value { get; set; }
     }
