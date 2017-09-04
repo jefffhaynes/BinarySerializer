95a3422e14f0a74906ae773083413c10615bdab2
Jeff Haynes <feedyurhed@gmail.com>
2017-09-04 13:38:06 -0400

    more test fields

---
+++ BinarySerializer.Editor/Test/PngColorType.cs
@@ -0,0 +1,11 @@
+﻿namespace BinarySerializer.Editor.Test
+{
+    public enum PngColorType : byte
+    {
+        Greyscale = 0,
+        Truecolor = 2,
+        Indexed = 3,
+        GreyscaleWithAlpha = 4,
+        TruecolorWithAlpha = 6
+    }
+}
--- BinarySerializer.Editor/Test/PngImageHeaderChunk.cs
+++ BinarySerializer.Editor/Test/PngImageHeaderChunk.cs
@@ -1,7 +1,28 @@
-﻿namespace BinarySerializer.Editor.Test
+﻿using BinarySerialization;
+
+namespace BinarySerializer.Editor.Test
 {
     public class PngImageHeaderChunk : PngChunk
     {
-        public byte[] Data { get; set; }
+        [FieldOrder(0)]
+        public int Width { get; set; }
+
+        [FieldOrder(1)]
+        public int Height { get; set; }
+
+        [FieldOrder(2)]
+        public byte BitDepth { get; set; }
+
+        [FieldOrder(3)]
+        public PngColorType ColorType { get; set; }
+
+        [FieldOrder(4)]
+        public byte CompressionMethod { get; set; }
+
+        [FieldOrder(5)]
+        public byte FilterMethod { get; set; }
+        
+        [FieldOrder(6)]
+        public byte InterlaceMethod { get; set; }
     }
 }
