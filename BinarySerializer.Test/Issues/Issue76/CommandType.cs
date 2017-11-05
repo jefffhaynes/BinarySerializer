0917d2dc9779cdfca3545888b608542b29e7f110
Jeff Haynes <feedyurhed@gmail.com>
2017-11-05 11:16:21 -0500

    cleanup

---
+++ BinarySerializer.Test/Issues/Issue76/CommandType.cs
@@ -0,0 +1,15 @@
+﻿namespace BinarySerialization.Test.Issues.Issue76
+{
+    public enum CommandType
+    {
+        SetAccessMode,
+
+        [SerializeAsEnum("mLMPsetscancfg")]
+        SetScanConfig,
+
+        [SerializeAsEnum("LMDscandatacfg")]
+        ScanDataConfig
+
+        // etc
+    }
+}
---
+++ BinarySerializer.Test/Issues/Issue76/Packet.cs
@@ -0,0 +1,12 @@
+﻿namespace BinarySerialization.Test.Issues.Issue76
+{
+    public class Packet
+    {
+        [FieldOrder(0)]
+        public uint Length { get; set; }
+
+        [FieldOrder(1)]
+        [FieldLength("Length")]
+        public PacketContent Content { get; set; }
+    }
+}
