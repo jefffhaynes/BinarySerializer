88e606f569745c583053a10d5763578dfdfe4bbb
Jeff Haynes <feedyurhed@gmail.com>
2017-12-22 08:15:04 -0500

    Tests for issue #80

---
+++ BinarySerializer.Test/Issues/Issue80/CustomField.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+
+namespace BinarySerialization.Test.Issues.Issue80
+{
+    public class CustomField : IBinarySerializable
+    {
+        private const byte NullValue = 0xff;
+
+        public string Value { get; set; }
+
+        public void Serialize(Stream stream, BinarySerialization.Endianness endianness, BinarySerializationContext serializationContext)
+        {
+            if (Value == null)
+            {
+                stream.WriteByte(NullValue);
+            }
+            else
+            {
+                var data = System.Text.Encoding.UTF8.GetBytes(Value);
+                stream.WriteByte((byte) data.Length);
+                stream.Write(data, 0, data.Length);
+            }
+        }
+
+        public void Deserialize(Stream stream, BinarySerialization.Endianness endianness, BinarySerializationContext serializationContext)
+        {
+            var length = stream.ReadByte();
+
+            if (length == 0xff)
+            {
+                Value = null;
+            }
+            else
+            {
+                var data = new byte[length];
+                stream.Read(data, 0, data.Length);
+                Value = System.Text.Encoding.UTF8.GetString(data);
+            }
+        }
+    }
+}
---
+++ BinarySerializer.Test/Issues/Issue80/Issue80Tests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace BinarySerialization.Test.Issues.Issue80
+{
+    public class Issue80Tests : TestBase
+    {
+        [Fact]
+        public void Test0Xff()
+        {
+            var expected = new CustomField();
+            var actual = Roundtrip(expected, new byte[] {0xff});
+            Assert.Null(actual.Value);
+        }
+
+        [Fact]
+        public void TestNormal()
+        {
+            var expected = new CustomField {Value = "hi"};
+            var actual = Roundtrip(expected);
+            Assert.Equal(expected.Value, actual.Value);
+        }
+    }
+}
