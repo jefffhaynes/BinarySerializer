03848ddd1ce9b942e003f5f4a4dbad9f1d5d13ff
Jeff Haynes <feedyurhed@gmail.com>
2018-02-21 18:07:35 -0500

    Issue94 test

---
+++ BinarySerializer.Test/Issues/Issue94/Issue94Tests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace BinarySerialization.Test.Issues.Issue94
+{
+    public class Issue94Tests : TestBase
+    {
+        class MainClass
+        {
+            [FieldOrder(0)] public int Count; // 3
+            [FieldOrder(1)] public int Offset; // 15
+            [FieldOrder(2), FieldCount("Count"), FieldOffset("Offset")] public List<Entries> Entries;
+        }
+
+        class Entries
+        {
+            [FieldOrder(0)] public int id;
+            [FieldOrder(1)] public int Offset;
+            [FieldOrder(2), FieldEncoding("ASCII"), FieldOffset(0)] public string String;
+        }
+
+        [Fact]
+        public void Test()
+        {
+            using (var file = new FileStream(@"Issues\Issue94\tst.file", FileMode.Open, FileAccess.Read))
+            {
+                var db = Deserialize<MainClass>(file);
+            }
+        }
+    }
+}
