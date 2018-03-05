fc14ebeeb2907c700d7c5adbab78cf2c6f87b656
Daniel Müller <daniel.mueller@copra-system.de>
2018-03-05 15:03:43 +0100

    Allow the serialization of enums with signed underlying types (int, long, short, sbyte) - does not throw an exception if value < 0

---
+++ BinarySerializer.Test/Enums/BaseTypeSignedEnumClass.cs
@@ -0,0 +1,6 @@
+﻿namespace BinarySerialization.Test.Enums {
+    public class BaseTypeSignedEnumClass
+    {
+        public BaseTypeSignedEnumValues Field { get; set; }
+    }
+}
---
+++ BinarySerializer.Test/Enums/BaseTypeSignedEnumValues.cs
@@ -0,0 +1,8 @@
+﻿namespace BinarySerialization.Test.Enums
+{
+    public enum BaseTypeSignedEnumValues : short
+    {
+        PositiveValue = 1,
+        NegativeValue = unchecked ((short)0xFFFF)
+    }
+}
