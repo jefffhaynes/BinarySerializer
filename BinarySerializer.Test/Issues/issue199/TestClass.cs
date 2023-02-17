3199b82e53b85e5ec52d9842586360b81ad445ca
zh3305 <zh3305@gmail.com>
2023-02-17 17:56:39 +0800

    feat: SerializeAs Support IValueConverter
    using
    ```
       [FieldOrder(0)]
        [SerializeAs(SerializedType.TerminatedString, StringTerminator = (char)0x20, ConverterType = typeof(HexStringToIntConvert))]
        public int DataAmount { get; set; }
    ```
    
    Refs: #199

---
+++ BinarySerializer.Test/Issues/issue199/TestClass.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using BinarySerialization;
+
+public class Issue199TestsClass
+{
+    [FieldOrder(0)]
+    [SerializeAs(SerializedType.TerminatedString, StringTerminator = (char)0x20)]
+    public string DataAmount { get; set; }
+
+    //数据列表
+    [FieldOrder(1)]
+    [FieldCount(nameof(DataAmount), ConverterType = typeof(HexStringToIntConvert))]
+    public List<DistData> DistDatas { get; set; }
+
+    public class DistData
+    {
+        [SerializeAs(SerializedType.TerminatedString, StringTerminator = (char)0x20)]
+        public string Data { get; set; }
+    }
+}
+
+public class OutputChannelInt
+{
+    [FieldOrder(0)]
+    [SerializeAs(SerializedType.TerminatedString, StringTerminator = (char)0x20, ConverterType = typeof(HexStringToIntConvert))]
+    public int DataAmount { get; set; }
+
+    //数据列表
+    [FieldOrder(1)]
+    [FieldCount(nameof(DataAmount))]
+    public List<DistDataInt> DistDatas { get; set; }
+
+    public class DistDataInt
+    {
+        [SerializeAs(SerializedType.TerminatedString, StringTerminator = (char)0x20, ConverterType = typeof(HexStringToIntConvert))]
+        public int Data { get; set; }
+    }
+}
+
+class HexStringToIntConvert : IValueConverter
+{
+    // Read
+    public object Convert(object value, object parameter, BinarySerializationContext context)
+    {
+        return (System.Convert.ToInt32((string)value, 16));
+    }
+
+    //Write
+    public object ConvertBack(object value, object parameter, BinarySerializationContext context)
+    {
+        return value switch
+        {
+            int intValue => intValue.ToString("X"),
+            long longValue => longValue.ToString("X"),
+            _ => (value)
+        };
+    }
+}
