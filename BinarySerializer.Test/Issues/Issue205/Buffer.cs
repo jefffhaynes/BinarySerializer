4762a724049ab7638d97cb993359223745b550db
Bevan Weiss <bevan.weiss@allianceautomation.com.au>
2023-05-03 18:02:19 +1000

    Test Case representing Issue #205 https://github.com/jefffhaynes/BinarySerializer/issues/205
    Signed-off-by: Bevan Weiss <bevan.weiss@allianceautomation.com.au>

---
+++ BinarySerializer.Test/Issues/Issue205/Buffer.cs
@@ -0,0 +1,26 @@
+﻿namespace BinarySerialization.Test.Issues.Issue205
+{
+    public class Segment
+    {
+        [FieldOrder(0)]
+        [FieldBitLength(3)]
+        public byte Type { get; set; }
+
+        [FieldOrder(1)]
+        public PortSegment PortSegment { get; set; }
+    }
+
+    public class PortSegment
+    {
+        [FieldOrder(0)]
+        [FieldBitLength(1)]
+        public bool ExtendedLinkAddress { get; set; }
+
+        [FieldOrder(1)]
+        [FieldBitLength(4)]
+        public byte Port { get; set; }
+
+        [FieldOrder(2)]
+        public byte LinkAddress;
+    }
+}
---
+++ BinarySerializer.Test/Issues/Issue205/Issue205Tests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BinarySerialization.Test.Issues.Issue205
+{
+    [TestClass]
+    public class Issue205Tests : TestBase
+    {
+        [TestMethod]
+        public void Test()
+        {
+            var expected = new Segment
+            {
+                Type = 0x00,
+                PortSegment = new PortSegment
+                {
+                    ExtendedLinkAddress = false,
+                    Port = 1,
+                    LinkAddress = 32,
+                },
+            };
+
+            var actual = Roundtrip(expected, new byte[] {0x01, 0x20});
+            Assert.AreEqual(expected.Type, actual.Type);
+            Assert.AreEqual(expected.PortSegment.ExtendedLinkAddress, actual.PortSegment.ExtendedLinkAddress);
+            Assert.AreEqual(expected.PortSegment.Port, actual.PortSegment.Port);
+            Assert.AreEqual(expected.PortSegment.LinkAddress, actual.PortSegment.LinkAddress);
+        }
+    }
+}
