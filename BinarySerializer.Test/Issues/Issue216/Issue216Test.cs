4a4f139a0d33850cfef54a6baec0ff47508faf35
Bevan Weiss <bevan.weiss@gmail.com>
2023-06-03 11:38:07 +1000

    As reported in Issue #216, when using a calculated binding, with a ValueGetter, the getter appears to be evaluted only once when initially converted to a TypeNode. This is unfortunately prior to any deserialized values being realised.
    This is a slightly hacky solution, but simply re-attempts to evaluate
    getters where this is the case.
    
    
    Signed-off-by: Bevan Weiss <bevan.weiss@gmail.com>

---
+++ BinarySerializer.Test/Issues/Issue216/Issue216Test.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BinarySerialization.Test.Issues.Issue216
+{
+    [TestClass]
+    public class Issue216Tests : TestBase
+    {
+        [TestMethod]
+        public void TestIssue216()
+        {
+            var expected = new Preview
+            {
+                ResolutionX = 2,
+                ResolutionY = 3,
+                Data = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
+                Empty = 0,
+            };
+
+            var actual = Roundtrip(expected, new byte[] { 2, 0, 0, 0, 3, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 0, 0, 0, 0 });
+            Assert.AreEqual(expected.ResolutionX, actual.ResolutionX);
+            Assert.AreEqual(expected.ResolutionY, actual.ResolutionY);
+            Assert.AreEqual(expected.Data.Length, actual.Data.Length);
+            for (int i = 0; i < expected.Data.Length; i++)
+                Assert.AreEqual(expected.Data[i], actual.Data[i]);
+        }
+    }
+}
